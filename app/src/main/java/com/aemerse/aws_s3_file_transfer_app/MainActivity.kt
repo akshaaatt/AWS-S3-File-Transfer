1c3b3ba34428b0c849b65919494d8bfa75836319
Akshat Tiwari <tiwariakshat03@gmail.com>
2022-07-13 13:53:34 +0530

    Initial commit

---
+++ app/src/main/java/com/aemerse/aws_s3_file_transfer_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.aemerse.aws_s3_file_transfer_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ aws_s3_file_transfer/src/main/java/com/aemerse/aws_s3_file_transfer/S3FileTransfer.kt
@@ -0,0 +1,5 @@
+package com.aemerse.aws_s3_file_transfer
+
+class S3FileTransfer {
+    
+}
